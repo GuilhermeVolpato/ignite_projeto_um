5a4767210ab7d9a7d3d579bb1af64d0e8b4ac907
GuilhermeVolpato <volpato2103@gmail.com>
2022-12-29 15:06:29 -0300

    componentização, scrollview, flatlist, componentização dinamica e mais

--- App.tsx
+++ App.tsx
@@ -1,11 +1,12 @@
-import {SafeAreaView} from "react-native"
-import Home from "./src/screens/Home" 
+import {SafeAreaView, StatusBar} from "react-native"
+import Home from "./src/screens/Home/index" 
 
 export default function App(){
 
   return(
     <SafeAreaView style={{flex: 1}}>
       <Home/>
+      <StatusBar/>
     </SafeAreaView>
   )
 }
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -1,49 +1,55 @@
 import { StyleSheet } from "react-native";
 
 export const styles = StyleSheet.create({
-    safeArea: {
-      flex: 1,
-      backgroundColor: '#131016',
-    },
-    container: {
-      padding: 24
-    },
-    eventName:{
-      color: '#FDFCFE',
-      fontSize: 24,
-      fontWeight: 'bold',
-      marginTop: 48
-    },
-    eventDate: {
-      color: '#6b6b6b',
-      fontSize: 16
-    },
-    textInput:{
-        height: 56,
-        backgroundColor: '#1F1E25',
-        borderRadius: 5,
-        color: '#FDFCFE',
-        padding:10,
-        fontSize: 16,
-        flex: 1,
-        marginRight: 10
-    },
-    buttomText:{
-        color: '#FFF',
-        fontSize: 15,
-    },
-    buttom:{
-        width: 56,
-        height: 56,
-        borderRadius: 5,
-        backgroundColor: '#31CF67',
-        alignItems: 'center',
-        justifyContent: 'center'
-    },
-    form:{
-        width: '100%',
-        flexDirection: 'row',
-        marginTop: 30,
-        marginBottom: 30
-    }
-  });
+  safeArea: {
+    flex: 1,
+    backgroundColor: "#131016",
+  },
+  container: {
+    flex: 1,
+    padding: 24,
+  },
+  eventName: {
+    color: "#FDFCFE",
+    fontSize: 24,
+    fontWeight: "bold",
+    marginTop: 48,
+  },
+  eventDate: {
+    color: "#6b6b6b",
+    fontSize: 16,
+  },
+  textInput: {
+    height: 56,
+    backgroundColor: "#1F1E25",
+    borderRadius: 5,
+    color: "#FDFCFE",
+    padding: 10,
+    fontSize: 16,
+    flex: 1,
+    marginRight: 10,
+  },
+  buttomText: {
+    color: "#FFF",
+    fontSize: 15,
+  },
+  buttom: {
+    width: 56,
+    height: 56,
+    borderRadius: 5,
+    backgroundColor: "#31CF67",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  form: {
+    width: "100%",
+    flexDirection: "row",
+    marginTop: 30,
+    marginBottom: 30,
+  },
+  emptyList: {
+    color: "#fff",
+    fontSize: 16,
+    textAlign: 'center'
+  },
+});
